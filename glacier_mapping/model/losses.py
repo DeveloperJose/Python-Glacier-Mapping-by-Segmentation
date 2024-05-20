11b53e336b4b53c32d24928dee83b78ed7c51a48
Jose Perez <josegperez@mail.com>
2024-05-20 15:49:09 -0600

    Testing unified loss

--- glacier_mapping/model/losses.py
+++ glacier_mapping/model/losses.py
@@ -8,13 +8,17 @@ Created on Thurs Sep 30 21:42:33 2021
 metrics
 """
 
-
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from skimage.filters import gaussian
 from torchvision.ops import sigmoid_focal_loss
+from numba import jit, njit
+from scipy.ndimage import gaussian_filter
+
+# import skimage.morphology as morph
+import cv2
 
 
 class diceloss(torch.nn.Module):
@@ -269,11 +273,16 @@ class customloss(torch.nn.modules.loss._WeightedLoss):
         self.masked = masked
         self.theta0 = theta0
         self.theta = theta
+        self.unified = unifiedloss(
+            act=act, label_smoothing=label_smoothing, outchannels=outchannels
+        )
 
         if self.outchannels == 2:
             print("customloss using masked dice")
 
     def forward(self, pred, target):
+        # TODO:
+        diceloss = self.unified(pred, target)
         if self.masked:
             mask = torch.sum(target, dim=1) == 1
         else:
@@ -321,15 +330,202 @@ class customloss(torch.nn.modules.loss._WeightedLoss):
 
         pred = pred.permute(0, 2, 3, 1)
         target = target.permute(0, 2, 3, 1)
-        diceloss = 1 - (
-            (2.0 * (pred * target)[mask].sum(dim=0) + self.smooth)
-            / (pred[mask].sum(dim=0) + target[mask].sum(dim=0) + self.smooth)
-        )
+        # diceloss = 1 - (
+        #     (2.0 * (pred * target)[mask].sum(dim=0) + self.smooth)
+        #     / (pred[mask].sum(dim=0) + target[mask].sum(dim=0) + self.smooth)
+        # )
 
-        # Only used masked dice loss when doing binary models
-        if self.outchannels == 2:
-            diceloss = diceloss * torch.tensor([0.0, 1.0]).to(diceloss.device)
-        elif self.outchannels == 3:
-            diceloss = diceloss * torch.tensor([0.0, 1.0, 1.0]).to(diceloss.device)
+        # # Only used masked dice loss when doing binary models
+        # if self.outchannels == 2:
+        #     diceloss = diceloss * torch.tensor([0.0, 1.0]).to(diceloss.device)
+        # elif self.outchannels == 3:
+        #     diceloss = diceloss * torch.tensor([0.0, 1.0, 1.0]).to(diceloss.device)
 
         return diceloss, boundaryloss
+
+
+# https://github.com/mlyg/boundary-uncertainty/blob/main/loss_functions.py
+# @njit(parallel=True)
+# @torch.jit.script
+def compute_res(seg, im_erode, im_dilate, alpha, beta):
+    # compute inner border and adjust certainty with alpha parameter
+    inner = seg - im_erode
+    inner = alpha * inner
+    # compute outer border and adjust certainty with beta parameter
+    outer = im_dilate - seg
+    outer = beta * outer
+    # combine adjusted borders together with unadjusted image
+    res = inner + outer + im_erode
+    return res
+
+
+accumulated_res = 0
+accumulated_res_idx = 0
+
+
+# Function to calculate boundary uncertainty
+def border_uncertainty_sigmoid(seg: torch.Tensor, alpha=0.9, beta=0.9):
+    global accumulated_res, accumulated_res_idx
+    """
+    Parameters
+    ----------
+    alpha : float, optional
+        controls certainty of ground truth inner borders, by default 0.9.
+        Higher values more appropriate when over-segmentation is a concern
+    beta : float, optional
+        controls certainty of ground truth outer borders, by default 0.1
+        Higher values more appropriate when under-segmentation is a concern
+    """
+    # start_time = timer()
+    # res = np.zeros_like(seg)
+    # check_seg = seg.astype(np.bool)
+    # seg = np.squeeze(seg)
+
+    res = torch.zeros_like(seg)
+    check_seg = seg.to(torch.bool)
+    seg = seg.squeeze()
+
+    if check_seg.any():
+        kernel = np.ones((3, 3), dtype=np.uint8)
+        seg_np = seg.cpu().numpy()
+        im_erode = torch.from_numpy(cv2.erode(seg_np, kernel, iterations=1)).to(
+            seg.device
+        )
+        im_dilate = torch.from_numpy(cv2.dilate(seg_np, kernel, iterations=1)).to(
+            seg.device
+        )
+        res = compute_res(seg, im_erode, im_dilate, alpha, beta)
+        return res
+    else:
+        return res
+
+
+# Enables batch processing of boundary uncertainty
+# def border_uncertainty_sigmoid_batch(y_true):
+#     return torch.from_numpy(np.array([border_uncertainty_sigmoid(y.cpu().numpy()) for y in y_true]).astype(np.float32))
+
+
+# https://github.com/mlyg/unified-focal-loss/blob/main/loss_functions.py
+# For later https://github.com/LIVIAETS/boundary-loss
+class unifiedloss(torch.nn.Module):
+    def __init__(
+        self,
+        weight=0.5,
+        delta=0.6,
+        gamma=0.5,
+        act=torch.nn.Sigmoid(),
+        label_smoothing=0.1,
+        outchannels=2,
+        boundary=True,
+    ):
+        super().__init__()
+        """
+        weight: float, optional represents lambda parameter and controls weight given to asymmetric Focal Tversky loss and asymmetric Focal loss, by default 0.5
+        delta : float, optional controls weight given to each class, by default 0.6
+        gamma : float, optional focal parameter controls the degree of background suppression and foreground enhancement, by default 0.5
+        """
+        self.weight = weight
+        self.delta = delta
+        self.gamma = gamma
+        self.act = act
+        self.label_smoothing = label_smoothing
+        self.outchannels = outchannels
+        self.boundary = boundary
+
+    def forward(self, pred, target):
+        if self.boundary:
+            global accumulated_res_idx, accumulated_res
+            device = target.device
+            # Convert batch to np
+            accumulated_res_idx = 0
+            accumulated_res = 0
+
+            # start_time = timer()
+            # target_np = target.permute(0, 2, 3, 1)
+            target_np = target
+            # print(f'\t\t{timer()-start_time:.5f}s for boundary')
+
+            # start_time = timer()
+            # target_np = target_np.cpu().numpy()
+            # print(f'\t\t{timer()-start_time:.5f}s for cpu numpy')
+
+            # start_time = timer()
+            target = [
+                border_uncertainty_sigmoid(y_true).cpu().numpy() for y_true in target_np
+            ]
+            # print(f'\t\t{timer()-start_time:.5f}s for list comprehension')
+
+            # start_time = timer()
+            # target = np.array(target)
+            # print(f'\t\t{timer()-start_time:.5f}s for np.array(target)')
+
+            # start_time = timer()
+            # target = torch.from_numpy(target).to(device).permute(0, 3, 1, 2)
+            target = np.asarray(target)
+            target = torch.from_numpy(target)
+            target = target.to(device)  # .permute(0, 3, 1, 2)
+            # print(target.shape)
+            # print(f'\t\t{timer()-start_time:.5f}s for last step')
+            # print(f'\t\t\t{accumulated_res/accumulated_res_idx:.5f}s for compute_res')
+
+        # Label Smoothing
+        # start_time = timer()
+        # target = target * (1 - self.label_smoothing) + self.label_smoothing / self.outchannels
+        # print(f'\t\t{timer()-start_time:.5f}s for smoothing')
+
+        # Masking
+        # start_time = timer()
+        # breakpoint()
+        mask = torch.sum(target_np, dim=1) == 1
+        # softmax so that predicted map can be distributed in [0, 1]
+        pred = self.act(pred)  # .permute(0,2,3,1)
+        # target = target.permute(0,2,3,1)
+        pred = pred.permute(0, 2, 3, 1)[mask]
+        target = target.permute(0, 2, 3, 1)[mask]
+        # print(f'\t\t{timer()-start_time:.5f}s for masking')
+
+        # Losses
+        # start_time = timer()
+        asymmetric_ftl = self.asymmetric_focal_tversky_loss(pred, target)
+        # print(f'\t\t{timer()-start_time:.5f}s for tversky')
+
+        # start_time = timer()
+        asymmetric_fl = self.asymmetric_focal_loss(pred, target)
+        # print(f'\t\t{timer()-start_time:.5f}s for focal')
+
+        # print(asymmetric_ftl, asymmetric_fl)
+
+        if self.weight is not None:
+            return (self.weight * asymmetric_ftl) + ((1 - self.weight) * asymmetric_fl)
+        else:
+            return asymmetric_ftl + asymmetric_fl
+
+    def asymmetric_focal_tversky_loss(self, y_pred: torch.Tensor, y_true: torch.Tensor):
+        if len(y_pred) == 0 or len(y_true) == 0:
+            raise ValueError("Empty tensor")
+        epsilon = 1e-07
+        y_pred = y_pred.clip(epsilon, 1.0 - epsilon)
+
+        tp = (y_true * y_pred).sum(dim=0)
+        fn = (y_true * (1 - y_pred)).sum(dim=0)
+        fp = ((1 - y_true) * y_pred).sum(dim=0)
+        dice_class = (tp + epsilon) / (
+            tp + self.delta * fn + (1 - self.delta) * fp + epsilon
+        )
+        back_dice = 1 - dice_class[0]
+        fore_dice = (1 - dice_class[1]) * torch.pow(1 - dice_class[1], -self.gamma)
+        return torch.stack([back_dice, fore_dice], -1).mean()
+
+    def asymmetric_focal_loss(self, y_pred, y_true):
+        if len(y_pred) == 0 or len(y_true) == 0:
+            raise ValueError("Empty tensor")
+        epsilon = 1e-07
+        y_pred = y_pred.clip(epsilon, 1.0 - epsilon)
+
+        cross_entropy = -y_true * torch.log(y_pred)
+        back_ce = torch.pow(1 - y_pred[:, 0], self.gamma) * cross_entropy[:, 0]
+        back_ce = (1 - self.delta) * back_ce
+
+        fore_ce = cross_entropy[:, 1]
+        fore_ce = self.delta * fore_ce
+        return torch.stack([back_ce, fore_ce], -1).sum(-1).mean()
