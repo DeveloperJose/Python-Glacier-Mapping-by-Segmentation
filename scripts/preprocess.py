ff151d43ebadc5588b8cebc55e61f5ac1fd51ea0
DevJ <josegperezdev@gmail.com>
2025-12-05 12:40:07 -0700

    Gen4 configs

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -150,7 +150,7 @@ if __name__ == "__main__":
         ]
     )
 
-    images = sorted(os.listdir(Path(conf.image_dir)))
+    images = sorted(Path(conf.image_dir).glob("*.tif"))
     idx = np.random.permutation(len(images))
     splits = {
         "test": sorted([images[i] for i in idx[: int(conf.test * len(images))]]),
